380f0f54c5e3785d5e80ea1918ab0201ffe8e588
psd <psd@whatfettle.com>
2016-01-11 11:06:23 +0000

    Stub finder

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,45 @@
 var express = require('express');
 var router = express.Router();
+// var openregister = require('openregister-client');
 
 router.get('/', function (req, res) {
   res.render('index');
 });
 
 
-// Example route: Passing data into a page
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
+var config = {
+  'register': {
+    'register': 'register'
+  }
+};
+
+// return register data in finder template format
+function get_list(register, filter) {
+
+  return [
+    { register: 'register', registry: 'cabinet-office', title: 'Registers maintained by HM Government' },
+    { register: 'fields', registry: 'cabinet-office', title: 'Fields used in registers maintained by HM Government' }
+  ]
+
+}
+
+function filters() {
+  return null;
+}
+
+router.get('/register/:register', function (req, res) {
+  var register = req.params.register;
+
+  var data = {
+    config: config[register],
+    title: 'Registers maintained by HM Government',
+    from: 'Cabinet Office',
+    register: 'register',
+    organisation: 'cabinet-office',
+    list: get_list(register, filters(req.params))
+  };
+  res.render(register, data);
 });
 
-// add your routes here
 
 module.exports = router;
